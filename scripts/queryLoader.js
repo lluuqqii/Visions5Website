1a2975e61634bfe96f3c8b980d6151b4c38bcf0a
Kavy Rattana <akaoctech@gmail.com>
2015-01-25 18:08:43 -0500

    current changes
    still need to do boostrap and google analytics

--- scripts/queryLoader.js
+++ scripts/queryLoader.js
@@ -13,7 +13,7 @@ var QueryLoader = {
 	overlay: "",
 	loadBar: "",
 	preloader: "",
-	items: new Array(),
+	items: [],
 	doneStatus: 0,
 	doneNow: 0,
 	selectorPreload: "body",
@@ -151,4 +151,4 @@ var QueryLoader = {
 			$(QueryLoader.preloader).remove();
 		});
 	}
-}
+};
